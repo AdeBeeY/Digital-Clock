dc219b13c90a5c47f8097411fddace77ee96222b
Adebayo Adewusi <adewusiadebayo@gmail.com>
2024-03-11 19:21:08 +0100

    make the clock display in 2 digits each

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ function getTime() {
   let mins = new Date().getMinutes();
   let sec = new Date().getSeconds();
 
+  //make the time diplay in 2digits
+  hour = String(hour).padStart(2, '0');
+  mins = String(mins).padStart(2, '0');
+  sec = String(sec).padStart(2, '0');
+
   digital = `${hour}:${mins}:${sec}`;
-  console.log(digital);
   display.innerHTML = digital;
 }
 
